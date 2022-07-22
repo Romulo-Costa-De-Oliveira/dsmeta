748235a1d28117400562baa934243cca92f85cce
Romulo Costa <romulocoliveirag@gmail.com>
2022-07-22 14:24:50 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,23 @@
+import axios from "axios";
 import notIcon from "../../assets/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import SalesCard from "../SalesCard";
 
-import './styles.css'
+import "./styles.css";
 
-const NotificationButton = () => {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    console.log("success");
+  });
+}
+
+const NotificationButton = ({ saleId }: Props) => {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={notIcon} alt="Notficar" />
     </div>
   );
